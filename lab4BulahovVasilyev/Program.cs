10257a905f81ade66ed62a971944ab46727a8f63
cocoxinido <nikita2004bulahov@gmail.com>
2023-11-30 23:29:12 +0700

    console

--- lab4BulahovVasilyev/Program.cs
+++ lab4BulahovVasilyev/Program.cs
@@ -10,7 +10,12 @@ namespace lab4BulahovVasilyev
     {
         static void Main(string[] args)
         {
-            ihdjvfiugsdfviugsdf
+            Console.WriteLine("Введите цифру операции..." + Environment.NewLine + "1| Сложение " + Environment.NewLine + "2| Вычитание " + Environment.NewLine + "3| Умножение " + Environment.NewLine + "4| Деление ");
+            string k = Console.ReadLine();
+            Console.WriteLine("Введите 1-ое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2-ое число:");
+            int b = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
